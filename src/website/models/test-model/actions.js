d554e8a23d230247be80fe6348ad1d4636405a7e
xristos1994 <44114413+xristos1994@users.noreply.github.com>
2019-11-15 02:32:06 +0200

    Test zip operatore usage

--- src/website/models/test-model/actions.js
+++ src/website/models/test-model/actions.js
@@ -3,3 +3,5 @@ import { Actions } from "@core/actions-engine";
 export const testAction = Actions("TEST", "TEST_ACTION");
 
 export const requestTestAction = Actions("TEST", "REQUEST_TEST_ACTION");
+
+export const requestTestAction1 = Actions("TEST", "REQUEST_TEST_ACTION1");
--- src/website/models/test-model/epics.js
+++ src/website/models/test-model/epics.js
@@ -1,12 +1,16 @@
 import { combineEpics, ofType } from "redux-observable";
 import {
-  //map,
-  mergeMap
+  map,
+  mergeMap,
+  tap,
+  repeat
   //withLatestFrom
 } from "rxjs/operators";
 
+import { zip } from "rxjs";
+
 import { request } from "@core/operators";
-import { testAction, requestTestAction } from "./actions";
+import { testAction, requestTestAction, requestTestAction1 } from "./actions";
 import * as services from "services";
 
 const epic = (action$, state$) => {
@@ -26,4 +30,35 @@ const requestTestEpic = (action$, state$) => {
   );
 };
 
-export const testEpic = combineEpics(epic, requestTestEpic);
+const requestTestEpic1 = (action$, state$) => {
+  return action$.pipe(
+    ofType(requestTestAction.type),
+    request(requestTestAction1, services.testService)
+    //mergeMap(() => [requestTestAction.succeeded({ value: 0 })])
+  );
+};
+
+const zipEpic = (action$, state$) => {
+  const stream1$ = action$.pipe(ofType(requestTestAction.succeeded.type));
+  const stream2$ = action$.pipe(ofType(requestTestAction1.succeeded.type));
+  const combined$ = zip(stream1$, stream2$);
+  console.log(combined$);
+
+  return combined$.pipe(
+    tap(([stream1, stream2]) => {
+      console.log([stream1.payload, stream2.payload]);
+    }),
+    //map(([stream1, stream2]) => {
+    map(payload => {
+      return testAction.succeeded({ test: 1 });
+    }),
+    repeat()
+  );
+};
+
+export const testEpic = combineEpics(
+  epic,
+  requestTestEpic,
+  requestTestEpic1,
+  zipEpic
+);
