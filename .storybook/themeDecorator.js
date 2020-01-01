14b4830ce11a6c9076c5ebb59818b21b2d1a5fd4
xristos1994 <44114413+xristos1994@users.noreply.github.com>
2020-01-01 04:10:50 +0200

    Code Correction

--- .storybook/themeDecorator.js
+++ .storybook/themeDecorator.js
@@ -1,6 +1,6 @@
-import React from "react";
-import theme from "./../src/theme";
-import { ThemeProvider } from "@material-ui/styles";
+import React from 'react';
+import theme from 'theme';
+import { ThemeProvider } from '@material-ui/styles';
 
 const ThemeDecorator = storyFn => (
   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
